2cff2d2ef806388123715590192d1277c200abcd
Dan <dansdeor@campus.technion.ac.il>
2021-05-22 16:57:02 +0300

    Updated 2.1

--- ex1/dry/2_1.c
+++ ex1/dry/2_1.c
@@ -1,71 +1,144 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 typedef struct node_t {
-    int x;
-    struct node_t* next;
+	int x;
+	struct node_t* next;
 } * Node;
 
-
 typedef enum {
-    SUCCESS = 0,
-    MEMORY_ERROR,
-    UNSORTED_LIST,
-    NULL_ARGUMENT,
+	SUCCESS = 0,
+	MEMORY_ERROR,
+	UNSORTED_LIST,
+	NULL_ARGUMENT,
 } ErrorCode;
 
 int getListLength(Node list);
 
 bool isListSorted(Node list);
 
+Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code);
 
-Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
+
+bool isListSorted(Node list)
 {
-    if (list1 == NULL || list2 == NULL || error_code == NULL) {
-        if (error_code == NULL) {
-            *error_code = NULL_ARGUMENT;
-        }
-        return NULL;
-    }
-    if (!isListSorted(list1) || !isListSorted(list2)) {
-        *error_code = UNSORTED_LIST;
-        return NULL;
-    }
-    Node head = NULL;
-    Node iterator = NULL;
-    while (list1 != NULL || list2 != NULL) {
-        Node added_node;
-        if (list1->x <= list2->x) {
-            added_node = list1;
-            list1 = list1->next;
-        } else {
-            added_node = list2;
-            list2 = list2->next;
-        }
-        Node new_node = malloc(sizeof(*new_node));
-        if (new_node == NULL) {
-            *error_code = MEMORY_ERROR;
-            return NULL;
-        }
-        new_node->x = added_node->x;
-        new_node->next = NULL;
-        if (head == NULL && iterator == NULL) {
-            head = new_node;
-            iterator = new_node;
-        } else {
-            iterator->next = new_node;
-            iterator = new_node;
-        }
-    }
-    if (list1 == NULL) {
-        iterator->next = list2;
-    } else {
-        iterator->next=list1;
-    }
-    *error_code = SUCCESS;
-    return head;
+	if (list == NULL) {
+		return true;
+	}
+	Node first = list;
+	Node second = list->next;
+	while (first != NULL && second != NULL) {
+		if (first->x > second->x) {
+			return false;
+		}
+		first = second;
+		second = second->next;
+	}
+	return true;
 }
 
 
+static Node copyNode(Node node)
+{
+	if (node == NULL) {
+		return NULL;
+	}
+	Node new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL) {
+		return NULL;
+	}
+	new_node->x = node->x;
+	new_node->next = NULL;
+	return new_node;
+}
+
+
+static void destroyList(Node list)
+{
+	Node iterator = list;
+	while (list != NULL) {
+		iterator = iterator->next;
+		free(list);
+		list = iterator;
+	}
+}
+
+
+Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
+{
+	if (list1 == NULL || list2 == NULL || error_code == NULL) {
+		if (error_code != NULL) {
+			*error_code = NULL_ARGUMENT;
+		}
+		return NULL;
+	}
+	if (!isListSorted(list1) || !isListSorted(list2)) {
+		*error_code = UNSORTED_LIST;
+		return NULL;
+	}
+	Node head, iterator = NULL;
+	while (list1 != NULL && list2 != NULL) {
+		Node new_node;
+		if (list1->x <= list2->x) {
+			new_node = copyNode(list1);
+			list1 = list1->next;
+		}
+		else {
+			new_node = copyNode(list2);
+			list2 = list2->next;
+		}
+		if (new_node == NULL) {
+			destroyList(head);
+			*error_code = MEMORY_ERROR;
+			return NULL;
+		}
+		if (iterator == NULL) {
+			head = new_node;
+			iterator = new_node;
+		}
+		else {
+			iterator->next = new_node;
+			iterator = new_node;
+		}
+	}
+	Node added_node = (list1 != NULL) ? list1 : list2;
+	while (added_node != NULL) {
+		Node new_node = copyNode(added_node);
+		if (new_node == NULL) {
+			destroyList(head);
+			*error_code = MEMORY_ERROR;
+			return NULL;
+		}
+		added_node = added_node->next;
+		iterator->next = new_node;
+		iterator = new_node;
+	}
+	*error_code = SUCCESS;
+	return head;
+}
+
+
+int main()
+{
+	struct node_t node1_3 = {8, NULL};
+	struct node_t node1_2 = {4, &node1_3};
+	struct node_t node1_1 = {2, &node1_2};
+
+	struct node_t node2_4 = {10, NULL};
+	struct node_t node2_3 = {9, &node2_4};
+	struct node_t node2_2 = {4, &node2_3};
+	struct node_t node2_1 = {1, &node2_2};
+
+	ErrorCode error_code;
+	Node head = mergeSortedLists(&node1_1, &node2_1, &error_code);
+	printf("ErrorCode: %d\n", error_code);
+	Node iterator = head;
+	while (iterator != NULL) {
+		printf("%d\n", iterator->x);
+		iterator = iterator->next;
+	}
+	destroyList(head);
+	return 0;
+}
