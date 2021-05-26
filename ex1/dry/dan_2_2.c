98f47160e58a0c2536d686f1ac4333b0fd5fd6a7
Dan <dansdeor@campus.technion.ac.il>
2021-05-26 17:29:57 +0300

    Changed error list

--- ex1/dry/dan_2_2.c
+++ ex1/dry/dan_2_2.c
@@ -24,19 +24,27 @@ char* foo(char* str, int* num)
 /*
  * 1. char* str2 declared not in the same place of usage
  * 2. int i declared not in the same place of usage
- * 3. char* str type is not const and str and num are not const pointers
- * 4. num pointing type should be size_t because of strlen return value
- * 5. no check if str is not NULL and num is not NULL
- * 6. changing num value and not pointed variable value (dangerous!!!)
- * 8. the allocation size is not correct and its better to use sizeof(char) to tell the programmer what we allocating
- * 9. no check after malloc the the allocation succeeded (dangerous!!!)
+ * 3. char* str type is not const and str and x are not const pointers
+ * 4. x pointing type should be size_t because of strlen return value
+ * 5. no check if str is not NULL and x is not NULL
+ * 6. changing x value and not pointed variable value (dangerous!!!)
+ * 8. the allocation size is not correct and its better to use sizeof(char) to tell the programmer what we are allocating (dangerous!!!)
+ * 9. no check after malloc if the allocation succeeded (dangerous!!!)
  * 10. no null setting at the end of str2
- * 11. in str the indexing is incorrect (supposed to be str[*num - i - 1] dangerous!!!)
- * 12. added to for braces (needs to check if this is important or not)
+ * 11. in str the indexing is incorrect (supposed to be str[*x - i - 1] dangerous!!!)
+ * 12. no braces after for (code convention)
  * 13. there is no reason to use both ifs if the other if checks the same statement
  * 14. added new line to both of printfs
- * 15. num is not conventional name
+ * 15. x is not conventional name
  * 16. str2 is not conventional name
+ * @ Working code issues:
+ * 6. changing x value and not pointed variable value (dangerous!!!)
+ * 8. the allocation size is not correct and its better to use sizeof(char) to tell the programmer what we are allocating (dangerous!!!)
+ * 11. in str the indexing is incorrect (supposed to be str[*x - i - 1] dangerous!!!)
+ * @ Code writing rules:
+ * 1. char* str2 declared not in the same place of usage
+ * 5. no check if str is not NULL and x is not NULL
+ * 9. no check after malloc if the allocation succeeded (dangerous!!!)
  */
 
 char* foo_updated(const char* const str, size_t* const str_length)
