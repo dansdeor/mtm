4fd4d3b84e67f1762bc375fd4d2dfcc0a10cf3b1
levi <levihorvitz@campus.technion.ac.il>
2021-06-16 14:52:20 +0300

    we finish the work

--- ex2/partA/examDetails.cpp
+++ ex2/partA/examDetails.cpp
@@ -1,34 +1,83 @@
 #include "examDetails.h"
 #include <iostream>
 #include <string>
+#include <utility>
+#include <math.h>
+#include <cfloat>
+
 using std::cout;
 using std::cin;
 using std::cerr;
 using std::endl;
 using std::string;
 
-class ExamDetails {
-	int num_course, month, day, time, exam_length;
-	string link;
-	char* test;
-
-public:
-	ExamDetails(int num_course, int month, int day, int time, int exam_length, string link );
-	~ExamDetails();
-	ExamDetails(const int &num_course,const int &month,const int &day,const int &time, const int &exam_length,const string &link);
-	ExamDetails operator=(const int &a);
-	ExamDetails operator-();
-	ExamDetails operator<(char const test);
-	string getLink(const string &link){};
-	string setLink(const string &newlink){};
-	void printDate(){};
-	static ExamDetails makeMatamExam(ExamDetails test){};
-	double distance(const ExamDetails& p) const;
-};
-
-ExamDetails::ExamDetails(int num_course, int month, int day, int time, int exam_length, string link) {
+bool mtm::ExamDetails::isTimeValid(double time)
+{
+	time *= 10;
+	int discrete_time = static_cast<int>(time);
+	return (discrete_time % 5 == 0) && (std::fabs(time - discrete_time) < DBL_EPSILON) && 0 <= time && time < 24;
 }
 
-int main() {
-	return 0;
+std::string mtm::ExamDetails::timeToText(double time)
+{
+	string text;
+	if (time < 10) {
+		text += "0";
+	}
+	int discrete_time = static_cast<int>(time);
+	text += std::to_string(discrete_time) + ":";
+	text += std::to_string((time - discrete_time) * 60);
+	return text;
+}
+
+mtm::ExamDetails::ExamDetails(int num_course, int month, int day, double time, double exam_length, const string& link)
+		: num_course(num_course), month(month), day(day), time(time), exam_length(exam_length), link(link)
+{
+	if (month < 1 || NUMBER_OF_MONTHS < month || day < 1 || DAYS_IN_MONTH < day) {
+		throw ExamDetails::InvalidDateException();
+	}
+	if (!isTimeValid(time)) {
+		throw ExamDetails::InvalidTimeException();
+	}
+	if (num_course < 0 || exam_length < 0) {
+		throw ExamDetails::InvalidArgsException();
+	}
+}
+
+
+std::string mtm::ExamDetails::getLink()
+{
+	return link;
+}
+
+void mtm::ExamDetails::setLink(const string& new_link)
+{
+	link = new_link;
+}
+
+
+int mtm::ExamDetails::operator-(const mtm::ExamDetails& exam) const
+{
+	return (month - exam.month) * DAYS_IN_MONTH + (day - exam.day);
+}
+
+bool mtm::ExamDetails::operator<(const mtm::ExamDetails& exam)
+{
+	int days = *this - exam;
+	return days < 0;
+}
+
+std::ostream& mtm::operator<<(std::ostream& os, const mtm::ExamDetails& exam)
+{
+	os << "Course Number: " << exam.num_course << endl;
+	os << "Time: " << exam.day << "." << exam.month << " at " << exam.timeToText(exam.time) << endl;
+	os << "Duration: " << exam.timeToText(exam.exam_length) << endl;
+	os << "Zoom Link: " << exam.link << endl;
+	return os;
+}
+
+
+mtm::ExamDetails mtm::ExamDetails::makeMatamExam(mtm::ExamDetails test)
+{
+	return mtm::ExamDetails(234124, 7, 28, 13, 3, "https://tinyurl.com/59hzps6m");
 }
--- ex2/partA/examDetails.h
+++ ex2/partA/examDetails.h
@@ -1,10 +1,61 @@
 #ifndef EXAM_DETAILS_H
 #define EXAM_DETAILS_H
 
-namespace mtm {
-	class examDetails {
+#include <string>
+#include <exception>
 
+namespace mtm {
+	class ExamDetails {
+	private:
+		int num_course;
+		int month;
+		int day;
+		double time;
+		double exam_length;
+		std::string link;
+
+		static const int DAYS_IN_MONTH = 30;
+		static const int NUMBER_OF_MONTHS = 12;
+
+		static bool isTimeValid(double time);
+
+		static std::string timeToText(double time);
+
+	public:
+		class InvalidDateException;
+
+		class InvalidTimeException;
+
+		class InvalidArgsException;
+
+		ExamDetails(int num_course, int month, int day, double time, double exam_length, const std::string& link = "");
+
+		~ExamDetails() = default;
+
+		ExamDetails(const ExamDetails& exam) = default;
+
+		ExamDetails& operator=(const ExamDetails& exam) = default;
+
+		std::string getLink();
+
+		void setLink(const std::string& new_link);
+
+		int operator-(const mtm::ExamDetails& exam) const;
+
+		bool operator<(const ExamDetails& exam);
+
+		friend std::ostream& operator<<(std::ostream& os, const ExamDetails& exam);
+
+		static ExamDetails makeMatamExam(ExamDetails test);
+	};
+
+	class ExamDetails::InvalidDateException : public std::exception {
+	};
+
+	class ExamDetails::InvalidTimeException : public std::exception {
+	};
+
+	class ExamDetails::InvalidArgsException : public std::exception {
 	};
 }
-
 #endif //EXAM_DETAILS_H
